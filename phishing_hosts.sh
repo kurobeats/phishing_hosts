3b733878774213629249ad54acad65e8a22fa11a
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2015-12-18 14:21:56 +0800

    Update phishing_hosts.sh

--- phishing_hosts.sh
+++ phishing_hosts.sh
@@ -10,6 +10,7 @@ echo "127.0.0.1    localhost.localdomain localhost" > hosts
 
 cat ./*.csv ./*.txt | grep -v phish > temp_file
 cat temp_file | cut -d "/" -f3 | sort -u >> hosts
+sed -i '/^\s*$/d' hosts
 sed -i -e 's/^/0.0.0.0 /' hosts
 sed -i -e 's/0.0.0.0 127.0.0.1/127.0.0.1/' hosts
 
