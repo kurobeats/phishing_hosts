9fd10a4da3e5e9d69e635cc7bcef1a12923af2b6
Anthony Cozamanis <anthony.cozamanis@chronicle.jp.net>
2016-01-05 13:28:59 +0800

    Update phishing_hosts.sh
    tricky (read: messy) trimming added

--- phishing_hosts.sh
+++ phishing_hosts.sh
@@ -9,7 +9,8 @@ rm ./xmlphishing.php
 echo "127.0.0.1    localhost.localdomain localhost" > hosts
 
 cat ./*.csv ./*.txt | grep -v phish > temp_file
-cat temp_file | cut -d "/" -f3 | sort -u >> hosts
+cat temp_file | cut -d "/" -f3 | rev | awk -F "." '{print $1 "." $2}' | rev | sort -u >> hosts
+
 sed -i '/^\s*$/d' hosts
 sed -i -e 's/^/0.0.0.0 /' hosts
 sed -i -e 's/0.0.0.0 127.0.0.1/127.0.0.1/' hosts
